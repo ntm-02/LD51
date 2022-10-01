c531f6b01e687af80d0cb62e84f4e9d784e35c0a
andycofsky <ajlitcofsky@gmail.com>
2022-10-01 17:25:30 -0400

    PlayerCombat changes

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class PlayerCombat : MonoBehaviour, IKillable
 {
     DamageableComponent damageableComponent;
+    [SerializeField] int damagePerHit = 10;
     [SerializeField] private GameObject damageLight;
 
     void Start()
     {
         damageableComponent = this.gameObject.AddComponent<DamageableComponent>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<DamageableComponent>(out DamageableComponent target))
+        {
+            damageableComponent.TakeDamage(damagePerHit);
+        }
     }
 
     public void Die()
