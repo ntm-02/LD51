33081c4f184f3fe77a7de11006215525543b59eb
Joseph Rousoss <jroussos01@gmail.com>
2022-10-02 11:38:36 -0400

    limiting how far the player can move

--- Assets/Scripts/Tile Scripts/MouseOnTile.cs
+++ Assets/Scripts/Tile Scripts/MouseOnTile.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MouseOnTile : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+public class MouseOnTile : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
 {
     GameObject playerTile;
     List<GameObject> path;
     Vector2 playerPos = Vector2.zero;
+
+    private bool mouseHovering = false;
     /*public void GotToTile()
     {
         StartCoroutine(FindObjectOfType<TileBasedMovement>().MovePlayer(Vector3.up));
@@ -22,41 +24,78 @@ public class MouseOnTile : MonoBehaviour, IPointerEnterHandler, IPointerClickHan
     void Update()
     {
         //playerPos = FindObjectOfType<TileBasedMovement>().getGridPos();
+        if (mouseHovering && !GameManager.IsPlayerMoving)
+        {
+            GeneratePathTrail();
+        }
+    }
+
+    private void GeneratePathTrail()
+    {
+        // clears the previous path trail
+        FindObjectOfType<TilePathFinding>().clearPathTrail();
+
+        // finding the tile with player on it
+        playerPos = FindObjectOfType<PlayerTileBasedMovement>().getGridPos();
+
+        // generating the new path with playerPos as start and the tile that was hovered over as the end
+        path = FindObjectOfType<TilePathFinding>().FindShortestPath(playerPos, transform.parent.parent.GetComponent<Tile>().gridPos);
+
+        PlayerTime.ResetTime();  // reset player time every time we move the pointer/mouse
+        foreach (GameObject g in path)
+        {
+            // if the path is within the player's allotted time, display as gray tinted tiles
+            if (PlayerTime.DecreaseTime(1f) != -1)  // decreases player time for each tile in path
+            {
+                g.GetComponent<SpriteRenderer>().color = Color.gray;
+            }
+            // else display as red tinted tiles
+            else
+            {
+                g.GetComponent<SpriteRenderer>().color = Color.red;
+            }
+        }
     }
 
    
     // on mouse hover, this will generate a path to the destination
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // clears the previous path trail
-        FindObjectOfType<TilePathFinding>().clearPathTrail();
-
-        // finding the tile with player on it
-        //GameObject[,] tileGrid = FindObjectOfType<TilePathFinding>().getGrid();
-
-        playerPos = FindObjectOfType<PlayerTileBasedMovement>().getGridPos();
-        //print(playerPos);
-        // generating the new path
-        path = FindObjectOfType<TilePathFinding>().FindShortestPath(playerPos, transform.parent.parent.GetComponent<Tile>().gridPos);
-        foreach (GameObject g in path)
+        if (!GameManager.IsPlayerMoving)
         {
-            
-            g.GetComponent<SpriteRenderer>().color = Color.gray;
+            GeneratePathTrail();
+        }
+        mouseHovering = true;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!GameManager.IsPlayerMoving)
+        {
+            StartCoroutine(MouseClickCoroutine());
         }
     }
 
-
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseHovering = false;
+    }
 
     public IEnumerator MouseClickCoroutine()
     {
-        if (!GameManager.IsPlayerMoving)  // probably unneccessary
+        if (!GameManager.IsPlayerMoving)  // probably unneccessary, this is checked in the method that calls it
         {
             foreach (GameObject g in path)
             {
+                // if the next tile is red stop moving--ran out of time
+                if (g.GetComponent<SpriteRenderer>().color == Color.red)
+                {
+                    break;
+                }
                 // player is currently moving
                 GameManager.IsPlayerMoving = true;
                 Vector2 playerPos = FindObjectOfType<PlayerTileBasedMovement>().getWorldPos();
-                //yield return new WaitForSeconds(0.5f); // wait one second before moving the player 
+
                 if (g.transform.position.y > playerPos.y)
                 {
                     FindObjectOfType<PlayerTileBasedMovement>().moveUp();
@@ -75,20 +114,12 @@ public class MouseOnTile : MonoBehaviour, IPointerEnterHandler, IPointerClickHan
                 }
                 yield return new WaitForSeconds(0.5f); // wait one second before moving the player
             }
+            // player no longer moving
             GameManager.IsPlayerMoving = false;
+
+            // Updating the path trail after movement: unfortunately this not for the tile am I am hovering over but the one I clicked
+            //GeneratePathTrail();
         }
     }
-
-
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (!GameManager.IsPlayerMoving)
-        {
-            StartCoroutine(MouseClickCoroutine());
-        }
-    }
-
-   
 
 }
