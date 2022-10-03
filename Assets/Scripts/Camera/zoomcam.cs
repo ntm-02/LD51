4d2a8eedd8945dc78f5489c60444b274ff2e6288
maidmage <46210759+glockmage@users.noreply.github.com>
2022-10-03 01:58:59 -0400

    sure why not ill push

--- Assets/Scripts/Camera/zoomcam.cs
+++ Assets/Scripts/Camera/zoomcam.cs
@@ -8,6 +8,7 @@ public class zoomcam : MonoBehaviour
     private float maxSize = 20f;
     private float sensitivity = 5f;
     private float camSize = 0f;
+    private bool canZoom = true;
 
     private Camera mainCam;
     // Start is called before the first frame update
@@ -21,10 +22,18 @@ public class zoomcam : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        camSize += Input.GetAxis("Mouse ScrollWheel") * sensitivity * -1f;
-        //print(Input.GetAxis("Mouse ScrollWheel"));
-        camSize = Mathf.Clamp(camSize, minSize, maxSize);
-        //print(camSize);
-        mainCam.orthographicSize = camSize;
+        if (canZoom)
+        {
+            camSize += Input.GetAxis("Mouse ScrollWheel") * sensitivity * -1f;
+            //print(Input.GetAxis("Mouse ScrollWheel"));
+            camSize = Mathf.Clamp(camSize, minSize, maxSize);
+            //print(camSize);
+            mainCam.orthographicSize = camSize;
+        }
+    }
+
+    public void SetCanZoom(bool zoom)
+    {
+        canZoom = zoom;
     }
 }
