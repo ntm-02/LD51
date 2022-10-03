3592b2a414128b905e8231ec0458ecdf552d9b7d
Joseph Rousoss <jroussos01@gmail.com>
2022-10-03 02:42:58 -0400

    imagine putting helpful commit messages

--- Assets/Scripts/Camera/pancam.cs
+++ Assets/Scripts/Camera/pancam.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class pancam : MonoBehaviour
 {
     Vector3 newPos;
+    GameObject Player;
     [SerializeField] float mouseSensitivity = -0.015f;
     [SerializeField] int max_x = int.MaxValue;
     [SerializeField] int max_y = int.MaxValue;
@@ -25,5 +26,12 @@ public class pancam : MonoBehaviour
             newPos = Input.mousePosition;
         }
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, min_x, max_x), Mathf.Clamp(transform.position.y, min_y, max_y), transform.position.z);
+    }
+
+    private IEnumerator Start()
+    {
+        Player = GameObject.Find("Player");
+        yield return new WaitForSeconds(0.1f);
+        transform.position = new Vector3(Player.transform.position.x + 5, Player.transform.position.y, -10f);
     }
 }
